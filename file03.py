dcde47cfc320e93a56b9d7404ba84495b9978512
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-30 17:38:08 +0500

    file-3

--- file03.py
+++ file03.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
-
+    f=open(data, mode='r')
+    s=f.read()
+    # a=s.split(',')
+    list1 = []
+    for i in s:
+        if i.isdigit():
+            list1.append(str(i))
+    return (list1)
+print(main('txt_file/data03.txt'))
 # Read data from file
