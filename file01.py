0c9a62ff620239026e376508f562257ecffb7041
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-30 17:36:48 +0500

    file-1

--- file01.py
+++ file01.py
@@ -7,4 +7,31 @@ def main(data:str):
         list: return answer
     """
 
+    f=open(data, mode='r')
+    
+    s=f.read()
+    a=s.split(',')
+    list1 = []
+    for i in a:
+        i=int(i)
+        list1.append(i)
+    return (list1)
+
+
+    file1 = open('myFile.txt', mode='w')
+    w = file1.write(str(list1))
+    # print(w)
+    # return w
+
+
+    
+
+print(main("txt_file/data01.txt"))
+
 # Read data from file
+
+
+
+
+
+
