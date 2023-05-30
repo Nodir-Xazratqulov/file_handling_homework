71500bdcd5542c2115e64e14d1806dadf2ea91da
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-30 17:38:35 +0500

    file-4

--- file04.py
+++ file04.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    f=open(data, mode='r')
+    a=f.read()
+    s=[]
+    for i in a:
+        if i.isalpha():
+            s.append(str(i))
+    s.insert(6,'\n')
+    return s
+print(main('txt_file/data04.txt'))
 # Read data from file
