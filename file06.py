20ec6bb181f1db8c6fdb36e16178413adf9e8ca1
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-30 17:39:48 +0500

    file-6

--- file06.py
+++ file06.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    f=open(data, mode='r')
+    s=f.read()
+    son=[]
+    for i in s.split('\n'):
+        leen=len(i)
+        son.append(leen)
+        # if leen>max_lenght:
+        #     max_lenght=leen 
+    return son
+print(main('txt_file/data06.txt'))
     
 # Read data from file
