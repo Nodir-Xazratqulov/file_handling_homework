8229250a324022a072fa4851494054a6a0670a6b
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-30 17:42:19 +0500

    file-10

--- file10.py
+++ file10.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    f=open(data, mode='r')
+    s=f.read()
+    max_length=0
+    for i in s.split('\n'):
+        print(i)
+        row_length=len(i)
+        if row_length>max_length:
+            max_length=row_length
+    return  max_length
+print(main('txt_file/data10.txt'))
 
-# Read data from file
